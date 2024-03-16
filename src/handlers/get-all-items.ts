5fca7a6f808bd505ff9daf4f17f9d4428b170d59
Yurii <yuriihr@wix.com>
2024-03-16 19:04:50 +0200

    Local database connection

--- src/handlers/get-all-items.ts
+++ src/handlers/get-all-items.ts
@@ -1,13 +1,4 @@
-// Create clients and set shared const values outside of the handler.
-
-// Create a DocumentClient that represents the query to add an item
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb';
-const client = new DynamoDBClient({});
-const ddbDocClient = DynamoDBDocumentClient.from(client);
-
-// Get the DynamoDB table name from environment variables
-const tableName = process.env.SAMPLE_TABLE;
+import mysql from 'mysql2/promise'
 
 /**
  * A simple example includes a HTTP get method to get all items from a DynamoDB table.
@@ -16,26 +7,30 @@ export const getAllItemsHandler = async (event: any) => {
     if (event.httpMethod !== 'GET') {
         throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
     }
-    // All log statements are written to CloudWatch
+
     console.info('received:', event);
 
-    // get all items from the table (only first 1MB data, you can use `LastEvaluatedKey` to get the rest of data)
-    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
-    // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
-    var params = {
-        TableName : tableName
-    };
+    const connection = await mysql.createConnection({
+        host: 'host.docker.internal',
+        user: 'root',
+        password: 'rr*UWhvUKeR8BL',
+        database: 'demo',
+    });
 
     try {
-        const data = await ddbDocClient.send(new ScanCommand(params));
-        var items = data.Items;
+        const [results, fields] = await connection.query(
+            'SELECT * FROM demo_table'
+        );
+
+        console.log(results); // results contains rows returned by server
+        console.log(fields); // fields contains extra meta data about results, if available
     } catch (err) {
-        console.log("Error", err);
+        console.log(err);
     }
 
     const response = {
         statusCode: 200,
-        body: JSON.stringify(items)
+        body: JSON.stringify([])
     };
 
     // All log statements are written to CloudWatch
